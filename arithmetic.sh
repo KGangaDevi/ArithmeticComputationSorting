2a24ae53f8b84a77769b08b4855dfd4297e00073
gangadevi <kakarlagangadevi@gmail.com>
2020-07-12 14:30:57 +0530

    Sorted Array in descending order

--- arithmetic.sh
+++ arithmetic.sh
@@ -9,6 +9,25 @@ op3=$(($c+$a/$b))
 op4=$(($a%$b+$c))
 for((i=1;i<=4;i++))
 do
-dict[((i))]=$(("op"$i))
+arr[((i))]=$(("op"$i))
 done
-echo ${dict[@]}
+echo ${arr[@]}
+for ((i = 1; i<=5; i++)) 
+do
+    for((j = 1; j<=5-i-1; j++)) 
+    do
+        if [ ${arr[j]} -gt ${arr[$((j+1))]} ] 
+     	  then
+            # swap 
+            temp=${arr[j]} 
+            arr[$j]=${arr[$((j+1))]}   
+            arr[$((j+1))]=$temp 
+        fi
+    done
+done
+echo ${arr[@]}
+echo "Sorted Array Descending order"
+for ((i=${#arr[@]};i>=1;i--))
+do
+	echo "${arr[i]}"
+done
